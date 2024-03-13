cb8c5574ef2cc7825107f1a8a2b22e00b7c58a8b
Julio Punkito <julesnoir@hotmail.fr>
2024-03-13 13:59:06 +0100

    feat: :sparkles: current host mesh look at mouse direction

--- scripts/class/person.gd
+++ scripts/class/person.gd
@@ -14,7 +14,6 @@ func _ready():
 
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	move_and_slide()
--- scripts/main.gd
+++ scripts/main.gd
@@ -18,23 +18,30 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	var look_at_direction = shoot_ray()
 	if current_host:
 		if Input.is_action_just_pressed("ui_accept"):
 			var parasite: Parasite = parasite_scn.instantiate()
 			parasite.connect("infect_person", _on_parasite_infect_person)
 			add_child(parasite)
-			parasite.global_transform.origin = current_host.global_transform.origin
-			var parasite_direction = shoot_ray().position - parasite.global_transform.origin
+			# Make the parasite start a little bit higher for a better shooting angle
+			parasite.global_transform.origin = current_host.global_transform.origin + Vector3.UP
+			var parasite_direction = (
+				(look_at_direction + Vector3.UP) - (parasite.global_transform.origin)
+			)
+			print(parasite_direction.normalized())
+			print("no normalize", parasite_direction)
+
 			parasite.shoot(parasite_direction.normalized())
 			current_host = null
 			return
-
-		# Get the input direction and handle the movement/deceleration.
+		# current_host.look_at(-shoot_ray().position, Vector3.UP)
+		current_host.mesh.rotation.y = lerp_angle(
+			current_host.rotation.y, atan2(look_at_direction.x, look_at_direction.z), 1
+		)  # Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
 		var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-		var direction = (
-			(current_host.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-		)
+		var direction = Vector3(input_dir.x, 0, input_dir.y).normalized()
 		if direction:
 			current_host.velocity.x = direction.x * SPEED
 			current_host.velocity.z = direction.z * SPEED
@@ -43,7 +50,7 @@ func _process(_delta):
 			current_host.velocity.z = move_toward(current_host.velocity.z, 0, SPEED)
 
 
-func shoot_ray() -> Dictionary:
+func shoot_ray() -> Vector3:
 	var mouse_pos: Vector2 = get_viewport().get_mouse_position()
 	var ray_length: int = 1000
 	var from: Vector3 = camera.project_ray_origin(mouse_pos)
@@ -52,10 +59,14 @@ func shoot_ray() -> Dictionary:
 	var ray_query: PhysicsRayQueryParameters3D = PhysicsRayQueryParameters3D.new()
 	ray_query.from = from
 	ray_query.to = to
-	return space.intersect_ray(ray_query)
+	ray_query.collision_mask = 2
+	var result: Dictionary = space.intersect_ray(ray_query)
+	if result.has("position"):
+		return result.position
+	return Vector3()
 
 
-func _on_parasite_infect_person(person:Person):
+func _on_parasite_infect_person(person: Person):
 	person.set_infected()
 	current_host = person
 	pass
