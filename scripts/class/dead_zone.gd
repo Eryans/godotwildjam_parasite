1561a7fa352156ff0f491dfb42518e0b9fd79e2d
Julio Punkito <julesnoir@hotmail.fr>
2024-03-14 19:15:42 +0100

    refactor: :recycle: dead zone size can be changed in the editor

--- scripts/class/dead_zone.gd
+++ scripts/class/dead_zone.gd
@@ -1,24 +1,39 @@
+@tool
 extends EventItem
 class_name DeadZone
 
 @export var is_active: bool = false
+@export var size: Vector3 = Vector3(1, .25, 1)
 
 @onready var collider: CollisionShape3D = $CollisionShape3D
+@onready var mesh: MeshInstance3D = $MeshInstance3D
 
 
 func _ready() -> void:
 	# EventManager.connect("")
 	connect("body_entered", _on_body_entered)
+
 	pass
 
 
+func _process(_delta) -> void:
+	if Engine.is_editor_hint():
+		mesh.mesh.size = size
+		collider.shape.size = size
+
+
 func activate() -> void:
-	print("set door open")
+	print("toggle dead zone")
 	is_active = !is_active
-	collider.disabled = is_active
-	visible = !is_active
+	if collider:
+		collider.disabled = is_active
+	if mesh:
+		mesh.visible = !is_active
 
 
 func _on_body_entered(body: Node3D) -> void:
-	if (body is Parasite) || (body is Person && body.current_state == body.person_state.INFECTED) && is_active:
+	if (
+		(body is Parasite)
+		|| (body is Person && body.current_state == body.person_state.INFECTED) && is_active
+	):
 		EventManager.parasite_in_dead_zone.emit()
