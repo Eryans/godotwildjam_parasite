9b7fafe45b66410529cd3664b6e0735dcb111c40
Julio Punkito <julesnoir@hotmail.fr>
2024-03-16 19:54:30 +0100

    feat: :sparkles: display interaction key when near interactable eventItems

--- scripts/class/event_triggerer.gd
+++ scripts/class/event_triggerer.gd
@@ -2,10 +2,21 @@ extends Area3D
 class_name EventTriggerer
 
 @export var event_items: Array[EventItem] = []
+@export var custom_label_text: String = ""
+@export var show_when_parasite:bool = false
+
+@onready var label: Label3D = $Label3D
 
 
 func _ready():
 	monitoring = true
+	if custom_label_text != "":
+		label.text = custom_label_text
+	else:
+		var key = InputMap.action_get_events("ui_action")[0].as_text()
+		label.text = "Press %s to interact" % key
+	connect("body_entered",_on_body_entered)
+	connect("body_exited",_on_body_exit)
 	pass
 
 
@@ -19,3 +30,12 @@ func _process(_delta):
 			if Input.is_action_just_pressed("ui_action"):
 				for item in event_items:
 					item.activate()
+
+func _on_body_entered(body:Node3D) -> void:
+	if label != null && (body is Person && body.current_state == body.person_state.INFECTED) || (body is Parasite && show_when_parasite):
+		label.visible = true
+
+func _on_body_exit(body:Node3D) -> void:
+	if label != null && (body is Person && body.current_state == body.person_state.INFECTED) || (body is Parasite && show_when_parasite):
+		label.visible = false
+	pass
