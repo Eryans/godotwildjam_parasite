64d10d35e956b0a369663e31c9f432a0b393eda8
Julio Punkito <julesnoir@hotmail.fr>
2024-03-16 14:42:47 +0100

    refactor: :recycle: eventTrigger activate on parasite collision

--- scripts/class/event_triggerer.gd
+++ scripts/class/event_triggerer.gd
@@ -12,6 +12,9 @@ func _ready():
 func _process(_delta):
 	var bodies: Array[Node3D] = get_overlapping_bodies()
 	for body in bodies:
+		if body is Parasite:
+			for item in event_items:
+				item.activate()
 		if body is Person && body.current_state == body.person_state.INFECTED:
 			if Input.is_action_just_pressed("ui_action"):
 				for item in event_items:
