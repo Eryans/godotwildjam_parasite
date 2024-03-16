c40efdfb2cd61ce89f5515a56018b60733d936ba
Julio Punkito <julesnoir@hotmail.fr>
2024-03-16 20:19:12 +0100

    feat: :sparkles: doors show what they need to be open

--- scripts/class/door.gd
+++ scripts/class/door.gd
@@ -8,9 +8,16 @@ class_name Door
 @onready var collider: CollisionShape3D = $CollisionShape3D
 @onready var mesh: MeshInstance3D = $MeshInstance3D
 @onready var interaction_area: Area3D = $DoorInteractionZone
+@onready var sprite_3D: Sprite3D = $Sprite3D
+
+@onready var has_key_texture = load("res://assets/materials/Textures/has_key.png")
+@onready var has_not_key_texture = load("res://assets/materials/Textures/has_not_key.png")
+@onready var terminal_icon = load("res://assets/materials/Textures/terminal_icon.png")
 
 
 func _ready() -> void:
+	interaction_area.connect("body_entered", _on_body_entered)
+	interaction_area.connect("body_exited", _on_body_exit)
 	pass
 
 
@@ -30,3 +37,21 @@ func _process(_delta):
 				if key_index != -1:
 					activate()
 					PlayerGlobal.player_keys.pop_at(key_index)
+
+
+func _on_body_entered(_body:Node3D) -> void:
+	if !is_open:
+		if use_key:
+			var key_index = PlayerGlobal.player_keys.find(key)
+			if key_index != -1:
+				sprite_3D.texture = has_key_texture
+			else :
+				sprite_3D.texture = has_not_key_texture
+		else:
+			sprite_3D.texture = terminal_icon
+		sprite_3D.visible = true
+
+
+func _on_body_exit(_body:Node3D) -> void:
+	sprite_3D.visible = false
+	pass
