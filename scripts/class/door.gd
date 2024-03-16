b7156f3875a8a971f645ad4ff53df0df4661a993
Julio Punkito <julesnoir@hotmail.fr>
2024-03-16 02:09:24 +0100

    style: :lipstick: updated openable door model

--- scripts/class/door.gd
+++ scripts/class/door.gd
@@ -4,6 +4,7 @@ class_name Door
 @export var is_open: bool = false
 
 @onready var collider:CollisionShape3D = $CollisionShape3D
+@onready var mesh:MeshInstance3D = $MeshInstance3D
 
 func _ready() -> void:
 	pass
@@ -13,4 +14,4 @@ func activate() -> void:
 	print("set door open")
 	is_open = !is_open
 	collider.disabled = is_open
-	visible = !is_open
+	mesh.visible = !is_open
